2a7b4c2368d0789b1650b1dda10de09768167128
David Okwacha <osweredavid@gmail.com>
2021-04-18 19:15:52 +0300

    styled lorem buttons

--- src/lorem.js
+++ src/lorem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function lorem() {
     return (
-        <div class="row m-5">
+        <div class="row my-5">
             <div class="col-md-6 blue height">
                 <h3 class='d-flex justify-content-center m-5'>
                     Lorem Ipsum
@@ -13,7 +13,7 @@ export default function lorem() {
                     ipsa quae ab illo inventore veritatis et quasi 
                 </p>
                 <div class='d-flex justify-content-center'>
-                    <button class="btn-danger ">Learn More</button>
+                    <button class="btn-danger sizebtn1 p-2">Learn More</button>
                 </div>
             </div>
             <div class="col-md-6 pink height">
@@ -26,7 +26,7 @@ export default function lorem() {
                 ipsa quae ab illo inventore veritatis et quasi
                 </p>
                 <div class='d-flex justify-content-center'>
-                    <button class="btn-primary ">Learn More</button>
+                    <button class="btn-primary sizebtn1 p-2">Learn More</button>
                 </div>
             </div>
         </div>
