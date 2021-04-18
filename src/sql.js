7502308b484475ed2697222b27694126dc521fa4
David Okwacha <osweredavid@gmail.com>
2021-04-18 18:57:44 +0300

    marginated 4th div of sql

--- src/sql.js
+++ src/sql.js
@@ -45,7 +45,7 @@ export default function sql() {
                         </p>
                     </div>
                 </div>
-                <div class="row col-sm">                    
+                <div class="row col-sm m-5">                    
                     <img src="../images/binary.jpg" alt="" class="img-fluid col-md-5 pixel"/>
                     <div class=" col-md-7">
                         <h4>SQL Code Organisation</h4>
