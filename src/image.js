d2d7699dc279fc254b1acfcabc5d75759594831e
David Okwacha <osweredavid@gmail.com>
2021-04-18 20:01:52 +0300

    styled the image logos of the languages

--- src/image.js
+++ src/image.js
@@ -2,25 +2,25 @@ import React from 'react'
 
 export default function image() {
     return (
-        <div class = " container row mx-auto">
-            <div class="col-2">
-                <img src='../images/Angular_full_color_logo.svg.png' alt="" class="img-thumbnail pixel" />
+        <div class = " container row mx-auto m-5">
+            <div class="col-2 ">
+                <img src='../images/Angular_full_color_logo.svg.png' alt="" class="img-fluid pixel" />
             </div>
 
-            <div class="col-2">
-                <img src="../images/css-3-logo.png" alt="" class="img-thumbnail pixel"/>
+            <div class="col-2 mx-5">
+                <img src="../images/css-3-logo.png" alt="" class="img-fluid pixel"/>
             </div>
 
-            <div class="col-2">
-                <img src="../images/html-logo.png" alt="" class="img-thumbnail pixel"/>
+            <div class="col-2 mx-3">
+                <img src="../images/html-logo.png" alt="" class="img-fluid pixel"/>
             </div>
 
-            <div class="col-2">
-                <img src="../images/600px-JavaScript-logo.png" alt="" class="img-thumbnail pixel"/>
+            <div class="col-2 mx-3">
+                <img src="../images/600px-JavaScript-logo.png" alt="" class="img-fluid pixel"/>
             </div>
 
-            <div class="col-2">
-                <img src="../images/saas.png" alt="" class="img-thumbnail pixel"/>
+            <div class="col-2 mx-2">
+                <img src="../images/saas.png" alt="" class="img-fluid pixel"/>
             </div>
         </div>
     )
