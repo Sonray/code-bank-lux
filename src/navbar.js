bf0670516d52585c3f714d3a2160c6a39d4714dc
David Okwacha <osweredavid@gmail.com>
2021-04-18 21:20:56 +0300

    styled the navbar

--- src/navbar.js
+++ src/navbar.js
@@ -7,20 +7,20 @@ export default function navbar() {
                 <h3 class="navbar-brand">CodeBank</h3>
             </div>
             <nav class="navbar-collapse d-flex navbar navbar-dark bg-primary" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item ">
-                        < a class="nav-link text-white" href="#">How it works</a>
+                <ul class="navbar-nav mx-4 float-md-left ">
+                    <li class="nav-item mx-2">
+                        < a class="nav-link text-white " href="#">How it works</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mx-2">
                         < a class="nav-link text-white" href="#">Blog</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mx-2">
                         < a class="nav-link text-white" href="#">Installations</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mx-2">
                         < a class="nav-link text-white" href="#">Resources</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mx-2">
                         <button class="btn btn-danger">Sign Up</button>
                     </li>
                 </ul>
