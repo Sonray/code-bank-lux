3c74ab54dae06654b1ecc0826535521a5a9d2fde
rosaliamuia <rosaliakaluki@gmail.com>
2021-04-19 16:10:32 +0300

    new changes to navbar

--- src/top.js
+++ src/top.js
@@ -3,22 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function top() {
     return (
-        <div className={'col-5 pinky' }>
-            <p>
-                <h1 className="text-center">
-                    A place for the most 
-                    useful code snipets 
-                    you need in your project.
-                </h1>
-            </p>
-            <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam,
-            eaque ipsa quae ab illo 
-            </p>
-            <div>
-                <button class=" btn-primary" >Get Started</button>
-                <button class=" btn-light text-primary" >Learn More</button>
+        
+        <div class='top'>
+            <div class='skewy bg-primary'></div>
+
+            <div class='pinktry length pinky '>
+                <div className={'col-6 pinky p-5 ' }>
+                    <p>
+                        <h2 className="">
+                            A place for the most 
+                            useful code snipets 
+                            you need in your project.
+                        </h2>
+                    </p>
+                    <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam,
+                    eaque ipsa quae ab illo 
+                    </p>
+                    <div>
+                        <button class=" btn-primary sizebtn1 p-2 " >Get Started</button>
+                        <button class=" btn-light text-primary sizebtn1 p-2 m-4 appear" >Learn More</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
