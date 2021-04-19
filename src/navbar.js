f7fb7bedbbb75a1f054397f493f7779f52800270
rosaliamuia <rosaliakaluki@gmail.com>
2021-04-19 16:11:02 +0300

    new changes to navbar

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,7 @@ export default function navbar() {
     return (
         <div class="navbar navbar-expand-lg ">
             <div class="navbar col-md-6 pinky px-5">
-                <h3 class="navbar-brand">CodeBank</h3>
+                <h3 class="navbar-brand sp">CodeBank</h3>
             </div>
             <nav class="navbar-collapse d-flex navbar navbar-dark bg-primary width" id="navbarNav">
                 <ul class="navbar-nav mx-4 float-md-left ">
